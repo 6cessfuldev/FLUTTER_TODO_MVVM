53d81f0f8e47a3a4bab01e7429605b09611da1fb
6cessfuldev <6cessfuldev@gmail.com>
2024-03-26 23:10:56 +0900

    ✨Feat(TodoRepository): 리포지토리 추가

---
+++ lib/repository/todo_repository.dart
@@ -0,0 +1,22 @@
+import 'package:todomvvm/dataSource/todo_local_data_source.dart';
+import 'package:todomvvm/model/todo.dart';
+
+class TodoRepository {
+  final TodoLocalDataSource _dataSource = TodoLocalDataSource();
+
+  Future<List<Todo>> getTodoList() {
+    return _dataSource.getTodoList();
+  }
+
+  Future<bool> saveTodoList(List<Todo> todoList) {
+    return _dataSource.saveTodoList(todoList);
+  }
+
+  Future<bool> addTodo(Todo item) {
+    return _dataSource.addTodo(item);
+  }
+
+  Future<bool> removeTodo(String id) {
+    return _dataSource.removeTodo(id);
+  }
+}
