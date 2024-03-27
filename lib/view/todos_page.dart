d1c8bb143fc27f528ceda93f15909b2df130541d
6cessfuldev <6cessfuldev@gmail.com>
2024-03-27 21:50:00 +0900

    ✨Feat(TodosPage): _showAddTodoDialog, _buildBody

--- lib/view/todos_page.dart
+++ lib/view/todos_page.dart
@@ -10,47 +10,97 @@ import 'package:todomvvm/viewModel/todo_state.dart';
 class TodosPage extends StatelessWidget {
   const TodosPage({super.key});
 
+  void _showAddTodoDialog(BuildContext context) async {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController contentController = TextEditingController();
+
+    void Function(TodoEvent event) addEvent = context.read<TodoBloc>().add;
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // 사용자가 다이얼로그 바깥을 탭해도 닫히지 않음
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Todo 항목 추가'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(hintText: "제목"),
+                ),
+                TextField(
+                  controller: contentController,
+                  decoration: const InputDecoration(hintText: "내용"),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('취소'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('추가'),
+              onPressed: () {
+                String uniqueId =
+                    '${DateTime.now().millisecondsSinceEpoch}-${Random().nextInt(999999)}';
+                Todo newTodo = Todo(
+                    id: uniqueId,
+                    title: titleController.text,
+                    description: contentController.text);
+
+                addEvent(TodoAdded(newTodo));
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildBody(context) {
+    return BlocBuilder<TodoBloc, TodoState>(builder: (context, state) {
+      var state = context.watch<TodoBloc>().state;
+      if (state is TodosLoadInProgress) {
+        return const Center(child: CircularProgressIndicator());
+      } else if (state is TodosLoadSuccess) {
+        final todos = state.todos;
+        return ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            final todo = todos[index];
+            return ListTile(
+              title: Text(todo.title),
+              leading: Checkbox(
+                value: todo.isCompleted,
+                onChanged: (bool? newValue) {
+                  context
+                      .read<TodoBloc>()
+                      .add(TodoUpdated(todo.copyWith(isCompleted: newValue!)));
+                },
+              ),
+            );
+          },
+        );
+      } else {
+        return const Center(child: Text('할 일 목록을 불러오는 데 실패했습니다.'));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<TodoBloc>().add(TodosLoaded());
-    });
-
     return Scaffold(
       appBar: AppBar(title: const Text('할 일 목록')),
-      body: BlocBuilder<TodoBloc, TodoState>(
-        builder: (context, state) {
-          if (state is TodosLoadInProgress) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is TodosLoadSuccess) {
-            final todos = state.todos;
-            return ListView.builder(
-              itemCount: todos.length,
-              itemBuilder: (context, index) {
-                final todo = todos[index];
-                return ListTile(
-                  title: Text(todo.title),
-                  leading: Checkbox(
-                    value: todo.isCompleted,
-                    onChanged: (bool? newValue) {
-                      context.read<TodoBloc>().add(
-                          TodoUpdated(todo.copyWith(isCompleted: newValue!)));
-                    },
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(child: Text('할 일 목록을 불러오는 데 실패했습니다.'));
-          }
-        },
-      ),
+      body: _buildBody(context),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          String uniqueId =
-              '${DateTime.now().millisecondsSinceEpoch}-${Random().nextInt(999999)}';
-          Todo newTodo = Todo(id: uniqueId, title: '새로운 할 일');
-          context.read<TodoBloc>().add(TodoAdded(newTodo));
+          _showAddTodoDialog(context);
         },
         child: const Icon(Icons.add),
       ),
